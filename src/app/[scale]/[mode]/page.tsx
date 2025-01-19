9933aa878a518edec45a90a2db2c861209784f05
Nick Mertens <nick.mertens@xero.com>
2025-01-19 17:02:32 +1300

    Link to "/" from H1

--- src/app/[scale]/[mode]/page.tsx
+++ src/app/[scale]/[mode]/page.tsx
@@ -2,6 +2,7 @@ import { ScaleSelector } from "@/app/components/ScaleSelector";
 import { Keyboard } from "@/app/components/Keyboard";
 import { Scale, ScaleModeName } from "@/app/musicTheory/basics";
 import { ModeSelector } from "@/app/components/ModeSelector";
+import Link from "next/link";
 
 export default async function Page({
   params,
@@ -11,9 +12,11 @@ export default async function Page({
   const { scale, mode } = await params;
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-xl font-bold">Tuatara</h1>
+        <h1 className="text-xl font-bold">
+          <Link href="/">Tuatara</Link>
+        </h1>
         <h2 className="text-lg">Scale</h2>
         <ScaleSelector selectedScale={scale} selectedMode={mode} />
         <h2 className="text-lg">Mode</h2>
@@ -21,9 +24,6 @@ export default async function Page({
 
         <Keyboard scale={scale} mode={mode} />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        &copy; Nick Mertens {new Date().getFullYear()}
-      </footer>
     </div>
   );
 }
