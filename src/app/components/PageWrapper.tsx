2d82afff3cbf47236b55fe164675c23ba61fe7e9
Nick Mertens <nick@thecrypt.co.nz>
2026-01-10 14:45:48 +1300

    Ensure components are centred correctly

--- src/app/components/PageWrapper.tsx
+++ src/app/components/PageWrapper.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 const PageWrapper = ({ children }: Props) => (
-  <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-8 md:p-20 font-[family-name:var(--font-geist-sans)]">
+  <div className="flex flex-col items-center align-middle min-h-screen p-8 pb-20 gap-8 md:p-20 font-[family-name:var(--font-geist-sans)]">
     <main className="flex flex-col gap-8 row-start-2 items-center">
       {children}
     </main>
